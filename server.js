62e246becdea0b70aaa67749db6d099aa88cad27
denis.ten <ten.denis2@wb.ru>
2023-01-08 10:55:47 +0500

    add usage node_env

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
+const port = 3000
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port: null })
+const app = next({ dev, hostname, port: dev ? 3000 : null })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
@@ -29,8 +30,8 @@ app.prepare().then(() => {
       res.statusCode = 500
       res.end('internal server error')
     }
-  }).listen(null, (err) => {
+  }).listen(dev ? port : null, (err) => {
     if (err) throw err
-    console.log(`> Ready on http://${hostname}`)
+    console.log(`> Ready on http://${hostname}:${port}`)
   })
 })
